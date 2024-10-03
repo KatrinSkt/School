7c882e9a213a282210ddf9419f50544348844971
Katrin_skt <katrinpivzavod@mail.ru>
2024-10-03 03:24:10 +0300

    done 4.3

--- src/test/java/ru/hogwarts/school/controller/StudentControllerWebMvcTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerWebMvcTest.java
@@ -3,7 +3,7 @@ package ru.hogwarts.school.controller;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+
 @WebMvcTest(StudentController.class)
 public class StudentControllerWebMvcTest {
 
